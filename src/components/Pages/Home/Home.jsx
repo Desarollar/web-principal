7b6fa09008bac7d7b874dd13050f349a2ffa848c
emybr <emybr82ar@gmail.com>
2024-06-10 20:05:44 -0300

    agrego tarjetas

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
+import Card from "../../Items/Card/Card";
 const Home = () => {
 
 return (
+    <>
     <div>SOY LA HOME</div>
+    <div className="flex flex-wrap gap-4 justify-evenly mt-10 mb-10 ">
+    <Card/>
+    <Card/>
+    <Card/>
+    </div>
+    </>
 )
 }
 
