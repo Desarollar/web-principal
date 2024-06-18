33bf6a4c071e681bcf2ddfd402f4bcb6f43743fe
emybr <emybr82ar@gmail.com>
2024-06-18 18:49:42 -0300

    agrego textos a equipo

--- src/components/Items/TeamSection/TeamSection.jsx
+++ src/components/Items/TeamSection/TeamSection.jsx
@@ -3,17 +3,24 @@
 const people = [
     {
         name: 'JORGE LOZANO',
-        role: 'Co-Founder / CEO / Desarrolladora Full Stack',
+        role: 'Desarrollador Full Stack',
         imageUrl:
             'https://camo.githubusercontent.com/e81b60886ee748ab86fde371adb733914716622c472dfd1b0285169a71ce3f2b/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f6471656a6c6a7569672f696d6167652f75706c6f61642f76313730323939353733382f696d616765732f33353136646634622d633137622d343431332d626361352d6666633836616434363332315f6c68327a6b672e6a7067',
         text:' Jorge se encarga de que nuestras aplicaciones se desplieguen de manera segura y eficiente. Con un enfoque en la automatización y la integración continua, Jorge garantiza que nuestro proceso de desarrollo sea fluido y sin interrupciones. Su experiencia en la gestión de infraestructuras en la nube y su conocimiento en herramientas como Docker, Kubernetes y Jenkins son esenciales para mantener la alta disponibilidad y escalabilidad de nuestras aplicaciones.'
     },
     {
         name: 'Emiliano Brizuela',
-        role: 'Desarrolladora Full Stack',
+        role: 'Desarrollador Full Stack',
         imageUrl:
             'https://avatars.githubusercontent.com/u/110181460?v=4',
         text: 'Emiliano es nuestro experto en desarrollo full stack, lo que significa que tiene la habilidad de trabajar tanto en el front-end como en el back-end de nuestras aplicaciones. Con una sólida formación en ingeniería de software y varios años de experiencia práctica, Emiliano se asegura de que nuestras aplicaciones no solo funcionen perfectamente, sino que también ofrezcan una experiencia de usuario excepcional. Su dominio de tecnologías como React, Node.js y Python es un activo invaluable para nuestro equipo.'
+    },
+    {
+        name: 'Belen Gallardo',
+        role: 'Desarrolladora Full Stack',
+        imageUrl:
+            'https://media.licdn.com/dms/image/D4D35AQGq3grHGBRUQg/profile-framedphoto-shrink_200_200/0/1687735167585?e=1719075600&v=beta&t=sw-Y9GQx_pE4k5oxf4OzC1VCczWMO3KlMNb3ZHqx5pE',
+        text: 'Belen aporta un enfoque centrado en el usuario a nuestro proceso de desarrollo. Como diseñadora y desarrolladora full stack, se asegura de que nuestras aplicaciones sean intuitivas y fáciles de usar. Con un ojo agudo para el diseño y una comprensión profunda de la psicología del usuario, Belen crea interfaces que no solo son estéticamente agradables, sino que también mejoran la satisfacción y retención del usuario.'
     },
     // More people...
 ]
